ab059e16c45d9bad027c0ce6bf82b293796dc6c4
Osalama1 <omarsalama@gmail.com>
2025-06-23 23:59:08 +0200

    Added AI Provider and AI Settings DocTypes with corrected structure

---
+++ ai_assistant/ontime_ai_assistant/doctype/ai_query/ai_query.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, osalama102@gmail.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("AI Query", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ ai_assistant/ontime_ai_assistant/doctype/ai_settings/ai_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, osalama102@gmail.com and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AISettings(Document):
+	pass
