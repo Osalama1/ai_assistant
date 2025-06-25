92ac2f68c14443f536ded1010c6eed9e289b5deb
Osalama1 <omarsalama@gmail.com>
2025-06-25 21:58:25 +0200

    Fix AI Provider Doctype to resolve link issues in AI Settings

--- ai_assistant/ontime_ai_assistant/doctype/ai_provider/ai_provider.js
+++ ai_assistant/ontime_ai_assistant/doctype/ai_provider/ai_provider.js
@@ -1,6 +1,8 @@
-frappe.ui.form.on("AI Provider", {
-	refresh: function(frm) {
+// Copyright (c) 2025, osalama102@gmail.com and contributors
+// For license information, please see license.txt
 
-	}
-});
+// frappe.ui.form.on("AI Provider", {
+// 	refresh(frm) {
 
+// 	},
+// });
--- ai_assistant/ontime_ai_assistant/doctype/ai_provider/ai_provider.py
+++ ai_assistant/ontime_ai_assistant/doctype/ai_provider/ai_provider.py
@@ -1,6 +1,9 @@
-import frappe
+# Copyright (c) 2025, osalama102@gmail.com and contributors
+# For license information, please see license.txt
+
+# import frappe
 from frappe.model.document import Document
+
 
 class AIProvider(Document):
 	pass
-
